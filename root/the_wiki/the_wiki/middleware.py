3a7b9a78f2e74feb237df46c6d3277d02974407d
TheTemptingSavior <TheTemptingSavior@protonmail.com>
2023-09-28 11:41:17 +0100

    add env var for auth everywhere

--- root/the_wiki/the_wiki/middleware.py
+++ root/the_wiki/the_wiki/middleware.py
@@ -1,4 +1,5 @@
 from django.http import HttpResponse
+from django.shortcuts import redirect
 
 
 class HealthCheckMiddleware:
@@ -9,3 +10,21 @@ class HealthCheckMiddleware:
         if request.path == "/health":
             return HttpResponse("ok")
         return self.get_response(request)
+
+
+class AuthEverywhereMiddleware:
+    def __init__(self, get_response):
+        self.get_response = get_response
+
+    def __call__(self, request):
+        # Bypass authenticated for the login route
+        if request.path == "/_accounts/login/" or request.path == "/admin/login/":
+            return self.get_response(request)
+
+        # user is not authenticated, redirect to the login page
+        if not request.user.is_authenticated:
+            return redirect('/_accounts/login/')
+
+        return self.get_response(request)
+
+

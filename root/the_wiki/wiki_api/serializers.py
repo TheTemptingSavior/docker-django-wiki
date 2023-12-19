59ec5dc436ca0033a52c72b6bd9a7442ef6a8d84
The Tempting Savior <TheTemptingSavior@protonmail.com>
2023-12-19 12:31:38 +0000

    move revisions to a sub-route of articles

--- root/the_wiki/wiki_api/serializers.py
+++ root/the_wiki/wiki_api/serializers.py
@@ -2,30 +2,32 @@ from django.contrib.auth.models import Group, User
 from rest_framework import serializers
 from wiki.models.article import Article, ArticleRevision
 
+from .apps import WikiApiConfig
+
 
 class UserSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = User
         fields = ['url', 'username', 'email', 'groups']
-        extra_kwargs = {'url': {'view_name': 'wiki_api:user-detail'}}
+        extra_kwargs = {'url': {'view_name': f'{WikiApiConfig.name}:user-detail'}}
 
 
 class GroupSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Group
         fields = ['url', 'name']
-        extra_kwargs = {'url': {'view_name': 'wiki_api:group-detail'}}
+        extra_kwargs = {'url': {'view_name': f'{WikiApiConfig.name}:group-detail'}}
 
 
 class ArticleSerializer(serializers.ModelSerializer):
     class Meta:
         model = Article
         fields = '__all__'
-        extra_kwargs = {'url': {'view_name': 'wiki_api:article-detail'}}
+        extra_kwargs = {'url': {'view_name': f'{WikiApiConfig.name}:articles-detail'}}
 
 
 class ArticleRevisionSerializer(serializers.ModelSerializer):
     class Meta:
         model = ArticleRevision
         fields = '__all__'
-        extra_kwargs = {'url': {'view_name': 'wiki_api:articlerevision-detail'}}
+        extra_kwargs = {'url': {'view_name': f'{WikiApiConfig.name}:articlerevisions-detail'}}
